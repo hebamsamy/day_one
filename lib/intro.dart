e9340540ecac3337c0f140f0bc347aa60264b741
hebamsamy <hebasamy655@gmail.com>
2023-01-31 19:49:18 +0200

    first commit

---
+++ lib/intro.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/material.dart';
+
+// class Person {
+//   String name;
+//   int age;
+//   // Person(this.age, this.name);
+//   Person({required this.age, required this.name});
+// }
+
+// void main() {
+//   // Person p = Person("hhhh", 30)
+//   Person p = Person(name: "hhhhh", age: 6);
+// }
